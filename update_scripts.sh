d10354ceb0c10b8a58e1cd535e0f875adbf147fc
Surim Oh <soh31@ucsc.edu>
2023-12-19 01:01:54 +0000

    Script for updating util scripts into a container

---
+++ update_scripts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONTAINERID=$1
+
+docker cp ./utilities.sh $CONTAINERID:/usr/local/bin
+docker cp ./run_clustering.sh $CONTAINERID:/usr/local/bin
+docker cp ./run_scarab.sh $CONTAINERID:/usr/local/bin
+docker cp ./run_scarab_allbench.sh $CONTAINERID:/usr/local/bin
+docker cp ./run_scarab_mode_4.sh $CONTAINERID:/usr/local/bin
+docker cp ./run_scarab_mode_4_allbench.sh $CONTAINERID:/usr/local/bin
+docker cp ./run_simpoint_trace.sh $CONTAINERID:/usr/local/bin
+docker cp ./run_trace_post_processing.sh $CONTAINERID:/usr/local/bin
+docker cp ./gather_cluster_results.py $CONTAINERID:/usr/local/bin
+docker cp ./gather_fp_pieces.py $CONTAINERID:/usr/local/bin
