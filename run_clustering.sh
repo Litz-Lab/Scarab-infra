3fc1f3cf690f4a168549562455773284d2590663
Mingsheng Xu <mxu61@ucsc.edu>
2024-02-06 15:42:58 -0800

    run_clustering.sh: seach every k within maxK

--- run_clustering.sh
+++ run_clustering.sh
@@ -9,10 +9,13 @@ cd $OUTDIR
 mkdir -p simpoints
 
 lines=($(wc -l $FPFILE))
-# round to nearest int
+# intended to round to nearest int
+# but in fact it is rouding down
 maxK=$(echo "(sqrt($lines)+0.5)/1" | bc)
+maxK=$(echo "$maxK*2" | bc)
 echo "fingerprint size: $lines, maxk: $maxK"
-spCmd="$tmpdir/simpoint -maxK $maxK -fixedLength off -numInitSeeds 1000 -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
+# spCmd="$tmpdir/simpoint -maxK $maxK -fixedLength off -numInitSeeds 1000 -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
+spCmd="$tmpdir/simpoint -k 1:$maxK -fixedLength off -numInitSeeds 1000 -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
 echo "cluster fingerprint..."
 echo "command: ${spCmd}"
 start=`date +%s`
