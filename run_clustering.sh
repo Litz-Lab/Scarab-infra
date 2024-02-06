b167138953097845a6a39bfde8c880eb65abb331
Mingsheng Xu <mxu61@ucsc.edu>
2024-02-06 15:47:27 -0800

    run_clustering.sh: correct maxK value and add comments

--- run_clustering.sh
+++ run_clustering.sh
@@ -10,11 +10,12 @@ mkdir -p simpoints
 
 lines=($(wc -l $FPFILE))
 # intended to round to nearest int
-# but in fact it is rouding down
+# but in fact it is just rouding down
 maxK=$(echo "(sqrt($lines)+0.5)/1" | bc)
-maxK=$(echo "$maxK*2" | bc)
 echo "fingerprint size: $lines, maxk: $maxK"
+# binary search with maxK
 # spCmd="$tmpdir/simpoint -maxK $maxK -fixedLength off -numInitSeeds 1000 -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
+# search every one with maxK
 spCmd="$tmpdir/simpoint -k 1:$maxK -fixedLength off -numInitSeeds 1000 -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
 echo "cluster fingerprint..."
 echo "command: ${spCmd}"
