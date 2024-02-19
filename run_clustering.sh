cd2b921127fd72f4d12d97d493418d0dc620037b
Mingsheng Xu <mxu61@ucsc.edu>
2024-02-18 23:53:42 -0800

    change clustering -numInitSeeds back to defalut value 5

--- run_clustering.sh
+++ run_clustering.sh
@@ -14,7 +14,8 @@ lines=($(wc -l $FPFILE))
 maxK=$(echo "(sqrt($lines)+0.5)/1" | bc)
 echo "fingerprint size: $lines, maxk: $maxK"
 # binary search with maxK
-spCmd="$tmpdir/simpoint -maxK $maxK -fixedLength off -numInitSeeds 1000 -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
+# numInitSeeds uses the defalut value 5 instead of 1000 -- run too long for big apps
+spCmd="$tmpdir/simpoint -maxK $maxK -fixedLength off -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
 # search every one with maxK
 # spCmd="$tmpdir/simpoint -k 1:$maxK -fixedLength off -numInitSeeds 1000 -loadFVFile $FPFILE -saveSimpoints $OUTDIR/simpoints/opt.p -saveSimpointWeights $OUTDIR/simpoints/opt.w -saveVectorWeights $OUTDIR/simpoints/vector.w -saveLabels $OUTDIR/simpoints/opt.l -coveragePct .99 &> $OUTDIR/simpoints/simp.opt.log"
 echo "cluster fingerprint..."
