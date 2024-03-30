392560ab6363b1c95456b67456724134d7a4be83
Surim Oh <soh31@ucsc.edu>
2024-03-29 19:30:10 -0700

    Fix for plot_figure script

--- isca2024_udp/plot/plot_figures.sh
+++ isca2024_udp/plot/plot_figures.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 SIM_PATH="$1"
 
-python plot_eval.py -d 'fig13.json' -b baseline/32 -s $SIM_PATH
-python plot_eval2.py -d 'fig14.15.json' -b baseline/32 -s $SIM_PATH
-python plot_btb.py -d 'btb.json' -s $SIM_PATH
-python plot_ftq.py -d 'ftq.json' -s $SIM_PATH
+python3 plot_eval.py -d 'fig13.json' -b baseline/32 -s $SIM_PATH
+python3 plot_eval2.py -d 'fig14.15.json' -b baseline/32 -s $SIM_PATH
+python3 plot_btb.py -d 'btb.json' -s $SIM_PATH
+python3 plot_ftq.py -d 'ftq.json' -s $SIM_PATH
